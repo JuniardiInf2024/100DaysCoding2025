d48454a440a2002d7642e9fd6d77be35f3254141
JuniardiInf2024 <juniardisalulemo@gmail.com>
2025-09-08 03:29:39 +0700

    Day1.java

--- Day1.java
+++ Day1.java
@@ -1,5 +1,7 @@
-public class juniardi{
-  public static void main [String args]{
-    system.out.println("Hello, world");
-  }
+package juniardi;
+import java.util.*;
+public class Coding1 {
+    public static void main(String[] args) {
+        System.out.println("Hello, world");
+    }
 }
