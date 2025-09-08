dcbd2703817b879a9c1bc972cefd6d5b39b8bb2f
JuniardiInf2024 <juniardisalulemo@gmail.com>
2025-09-08 17:30:53 +0700

    Day1.java

--- Day1.java
+++ Day1.java
@@ -1,8 +1,6 @@
-package juniardi; //Struktur Dasar yang pertama meliputi deklarasi package
-import java.util.*; //Struktur Dasar yang kedua meliputi impor library
-public class Coding1 { //Struktur Dasar yang ketiga meliputi  pembuatan class
-    public static void main(String[] args) { //Struktur Dasar yang keempat adalah method main
-        int angka = 27; //Struktur Dasar yang kelima adalah deklarasi variabel
-        System.out.println("Hello, world"); //Program Hello world
+package juniardi; 
+public class Coding1 { 
+    public static void main(String[] args) { 
+        System.out.println("Hello, world"); 
     }
 }
