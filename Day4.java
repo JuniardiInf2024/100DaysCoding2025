cbbc5fd7426237c1ba543184b9b3b2edc8abf32d
JuniardiInf2024 <juniardisalulemo@gmail.com>
2025-09-11 19:21:45 +0800

    Day4.java

---
+++ Day4.java
@@ -0,0 +1,21 @@
+package juniardi;
+
+public class Coding3 {
+    public static void main(String[] args) {
+        // \n = membuat baris baru
+        System.out.println("Halo, nama saya Juniardi\nSaya sedang belajar Java.");
+
+        // \t = memberi tab (spasi lebar)
+        System.out.println("Nama:\tJuniardi");
+        System.out.println("Kelas:\tInformatika 2025");
+
+        // \" = menampilkan tanda kutip ganda
+        System.out.println("Saya suka belajar \"Java Programming\"");
+
+        // \\ = menampilkan tanda backslash
+        System.out.println("Folder tersimpan di C:\\Users\\Juniardi\\Documents");
+
+        // \b = backspace (menghapus 1 karakter sebelum)
+        System.out.println("Hello\b!");
+    }
+  }
