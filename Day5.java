75cfd8d91239fcc4eb198bdd8732d2c1039a6015
JuniardiInf2024 <juniardisalulemo@gmail.com>
2025-09-12 18:55:55 +0800

    Create Day5.java

---
+++ Day5.java
@@ -0,0 +1,15 @@
+package juniardi;
+
+public class Coding5 {
+    public static void main(String[] args) {
+        byte angka_byte = 27; // byte range = -128 sd 127
+        short angka_short = 6;// short range = -32.768 sd 32.767
+        int angka_int = 2005; // int range = -2^31 sd 2^31-1
+        long angka_long = 270605; // long range = -2^63 sd 2^63-1
+
+        System.out.println(angka_byte);
+        System.out.println(angka_short);
+        System.out.println(angka_int);
+        System.out.println(angka_long);
+    }
+}
