88ccd221ab18f68bac6748ec7a34d781cba7c0f9
JuniardiInf2024 <juniardisalulemo@gmail.com>
2025-09-13 19:05:51 +0800

    Day6.java

---
+++ Day6.java
@@ -0,0 +1,11 @@
+package juniardi;
+
+public class Coding6 {
+    public static void main(String[] args) {
+        double gaji = 27.062.005; //type data double digunakan untuk angka desimal
+        float harga = 2.005f; // pakai 'f' agar dikenali sebagai float, juga tipe data angka desimal
+
+        System.out.println(gaji);
+        System.out.println(harga);
+    }
+}
