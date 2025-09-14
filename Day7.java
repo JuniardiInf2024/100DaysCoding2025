97ad03fb1df76f620bbd1682f419b6b6267a1f7c
JuniardiInf2024 <juniardisalulemo@gmail.com>
2025-09-14 18:59:26 +0800

    Day7.java

---
+++ Day7.java
@@ -0,0 +1,11 @@
+package juniardi;
+
+public class Coding7 {
+    public static void main(String[] args) {
+        char inisial = 'J'; 
+        boolean kondisi =  true; 
+
+        System.out.println("Inisial : " + inisial);
+        System.out.println("Kondisi : " + kondisi); 
+    }
+}
