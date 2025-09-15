1116531ed64409556bffba96d5efb787466c4680
JuniardiInf2024 <juniardisalulemo@gmail.com>
2025-09-15 16:28:09 +0800

    Day8.java

---
+++ Day8.java
@@ -0,0 +1,8 @@
+package juniardi;
+public class Coding8 {
+    public static void main(String[] args) {
+        //Tipe data string untuk teks
+        String perkenalan = "Nama saya adalah Juniardi";
+        System.out.println(perkenalan);
+    }
+}
