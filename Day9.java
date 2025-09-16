b3f80c5544cc8082a75980ad07bf7f7e38f77eff
JuniardiInf2024 <juniardisalulemo@gmail.com>
2025-09-16 20:33:19 +0800

    Day9.java

---
+++ Day9.java
@@ -0,0 +1,12 @@
+package juniardi;
+public class Coding8 {
+    public static void main(String[] args) {
+        
+        int angka; //Deklarasi variabel
+        angka = 27; //Inisialisasi variabel
+        System.out.println("Sebelum diupdate : " + angka);
+
+        angka = 29; //Update variabel
+        System.out.println("Setelah diupdate : " + angka);
+    }
+}
