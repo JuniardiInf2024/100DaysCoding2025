bc2eef566ade62d10a9e482302cabb014d8de198
JuniardiInf2024 <juniardisalulemo@gmail.com>
2025-09-18 03:19:01 +0800

    Day11.java

---
+++ Day11.java
@@ -0,0 +1,10 @@
+package juniardi;
+import java.util.Scanner; //Import kelas Scanner
+public class Coding11 {
+    public static void main(String[] args) {
+        Scanner juni = new Scanner(System.in);
+        System.out.print("Masukkan Nama Anda : ");
+        String nama = juni.nextLine();
+        System.out.print("Nama Anda adalah " + nama);
+    }
+}
