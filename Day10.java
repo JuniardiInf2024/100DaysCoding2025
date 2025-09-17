10b6bf39721e8fd353f50c79b8a62a513fdee394
JuniardiInf2024 <juniardisalulemo@gmail.com>
2025-09-17 19:34:19 +0800

    Day10.java

---
+++ Day10.java
@@ -0,0 +1,8 @@
+package juniardi;
+
+public class Coding10{
+    public static void main(String[] args) {
+        final int angka = 27; //Nilai variabel tidak dapat diupdate karena merupakan variabel konstanta
+        System.out.println(angka);
+    }
+}
