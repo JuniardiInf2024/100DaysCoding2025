8f4a29f8d1f4756dedbffc91c39419ab3c999737
Juna <juniardisalulemo@gmail.com>
2025-09-22 00:02:20 +0800

    Operator Aritmatika Penjumlahan (+) dan Pengurangan (-)

---
+++ Day15.java
@@ -0,0 +1,17 @@
+package juniardi;
+
+public class Coding15 {
+    public static void main(String[] args) {
+        
+        double angka1 = 10.5;
+        double angka2 = 5.5;
+
+        // Operasi penjumlahan
+        double hasilPenjumlahan = angka1 + angka2;
+        System.out.println("Hasil Penjumlahan: " + hasilPenjumlahan);
+
+        // Operasi pengurangan
+        double hasilPengurangan = angka1 - angka2;
+        System.out.println("Hasil Pengurangan: " + hasilPengurangan);
+    }
+}
