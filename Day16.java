123dff9b5bbebe9cd064238927f2bfcd218018d8
Juna <juniardisalulemo@gmail.com>
2025-09-23 14:05:09 +0800

    Operator Aritmatika Perkalian (*) dan Pembagian (/)

---
+++ Day16.java
@@ -0,0 +1,12 @@
+package juniardi;
+public class Coding16{
+    public static void main(String[] args) {
+        
+        double angka1 = 10;
+        double angka2 = 4;
+
+        // Menampilkan hasil
+        System.out.println("Hasil Perkalian: " + angka1 * angka2);
+        System.out.println("Hasil Pembagian: " + angka1 / angka2);
+    }
+}
