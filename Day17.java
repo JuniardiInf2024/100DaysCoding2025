b5eb93d8c98a847cfeb882d3c1ff8cf9a33b2464
Juna <juniardisalulemo@gmail.com>
2025-09-24 20:17:54 +0800

    Operator Aritmatika Modulus atau Sisa Bagi (%)

---
+++ Day17.java
@@ -0,0 +1,9 @@
+package juniardi;
+public class Coding 17 {
+    public static void main(String[] args) {
+        int a = 27;
+        int b = 6;
+      
+        System.out.println("Hasil sisa bagi : " + a % b); //Hasil 3
+    }
+}
