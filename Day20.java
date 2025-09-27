981454109484dcc8bd18bc9d78dc544702390819
Juna <juniardisalulemo@gmail.com>
2025-09-27 14:55:13 +0800

    Mengubah Tipe Data Primitif menjadi String

---
+++ Day20.java
@@ -0,0 +1,20 @@
+package juniardi;
+
+public class Coding20 {
+    
+    public static void main(String[] args) {
+        int angka = 27;
+        double pecahan = 27.6;
+        boolean kondisi = true;
+        
+        String str1 = Integer.toString(angka);
+        String str2 = Double.toString(pecahan);
+        String str3 = Boolean.toString(kondisi);
+        
+        System.out.println("String dari int " + str1);
+        System.out.println("String dari double " + str2);
+        System.out.println("String dari boolean " + str3);
+        
+    }
+    
+}
