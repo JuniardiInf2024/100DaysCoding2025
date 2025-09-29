3345d4f50c151d5aec93ab13fb63bdd7a3bcc449
Juna <juniardisalulemo@gmail.com>
2025-09-29 17:30:35 +0800

    Create Day22.java

---
+++ Day22.java
@@ -0,0 +1,11 @@
+package juniardi ;
+
+public class Coding22{
+    public static void main(String[] args) {
+        int sisi = 27;
+        int luas = sisi * sisi;
+
+        System.out.println("Sisi persegi: " + sisi);
+        System.out.println("Luas persegi: " + luas);
+    }
+}
