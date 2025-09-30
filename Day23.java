840c0c242c36b2f784e896655d3c6c2323a91340
Juna <juniardisalulemo@gmail.com>
2025-09-30 17:51:18 +0800

    Latihan: Program Luas Persegi Panjang

---
+++ Day23.java
@@ -0,0 +1,9 @@
+package juniardi ;
+public class Coding23 {
+    public static void main(String[] args) {
+        int panjang = 27;
+        int lebar = 6;
+        
+        System.out.println("Luas persegi panjang : " + panjang * lebar);
+    }
+}
