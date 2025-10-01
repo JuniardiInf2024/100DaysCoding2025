d2e8f0b395bcd5643e482d47dbe22634af6801c8
Juna <juniardisalulemo@gmail.com>
2025-10-01 15:48:16 +0800

    Latihan: Program Luas Lingkaran

---
+++ Day24.java
@@ -0,0 +1,9 @@
+package juniardi;
+public class Coding24 {
+    public static void main(String[] args) {
+        int r = 6; //r adalah jari-jari lingkaran
+        final double π = 3.14; //π adalah nilai konstanta dalam lingkaran
+        
+        System.out.println("Luas lingkaran adalah " + (π * (r * r)));
+    }
+}
