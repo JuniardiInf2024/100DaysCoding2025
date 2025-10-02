934900c874ec38e44ed2d212d5a427384b0f6e12
Juna <juniardisalulemo@gmail.com>
2025-10-02 17:49:49 +0700

    Operator Increment dan Decrement (++, --)

---
+++ Day25.java
@@ -0,0 +1,12 @@
+package juniardi;
+public class Coding25 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5 ; i++) {
+            System.out.println("nilai i : " + i);
+        }
+        System.out.println("");
+        for (int j = 5; j >= 1; j--) {
+            System.out.println("nilai j : " + j);
+        }
+    }
+}
