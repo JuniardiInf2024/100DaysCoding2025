eb134af6c5bc2d6df70b729518319a2ffeb8b5eb
Juna <juniardisalulemo@gmail.com>
2025-10-03 18:22:43 +0800

    Operator Penugasan (=, +=, -=, dll)

---
+++ Day26.java
@@ -0,0 +1,26 @@
+package juniardi;
+public class Coding26{
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        System.out.println("Nilai awal:");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        a += b;
+        System.out.println("a += b: " + a);
+
+        a -= b;
+        System.out.println("a -= b: " + a);
+
+        a *= b;
+        System.out.println("a *= b: " + a);
+
+        a /= b;
+        System.out.println("a /= b: " + a);
+
+        a %= b;
+        System.out.println("a %= b: " + a);
+    }
+}
