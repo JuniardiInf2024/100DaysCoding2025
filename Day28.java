9478463c92e6609a81907c14112df3e8fc82e686
Juna <juniardisalulemo@gmail.com>
2025-10-05 23:29:53 +0800

    Operator perbandingan == dan !=

---
+++ Day28.java
@@ -0,0 +1,10 @@
+package juniardi;
+
+public class Coding28 {
+    public static void main(String[] args) {
+        int angka1 = 5;
+        int angka2 = 6;
+        System.out.println(angka1 == angka2);//output false
+        System.out.println(angka1 != angka2);//output true
+    }
+}
