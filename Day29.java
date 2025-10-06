dfea70c50153d2bba4d439dcc9f99fb578d1fe38
Juna <juniardisalulemo@gmail.com>
2025-10-06 23:02:19 +0800

    Operator Perbandingan < dan >

---
+++ Day29.java
@@ -0,0 +1,10 @@
+package juniardi;
+public class Coding29 {
+    public static void main(String[] args) {
+        int a = 27;
+        int b = 6;
+
+        System.out.println("a > b : " + (a > b));
+        System.out.println("a < b : " + (a < b));
+    }
+}
