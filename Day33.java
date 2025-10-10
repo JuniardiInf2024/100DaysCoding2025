6aa494b39bf709531c7af39660da98e89b6fdba8
Juna <juniardisalulemo@gmail.com>
2025-10-10 22:37:20 +0800

    Operator Logika NOT (!)

---
+++ Day33.java
@@ -0,0 +1,7 @@
+package juniardi;
+public class Day333 {
+    public static void main(String[] args) {
+    boolean lulus = true;
+    System.out.println("Apakah Anda lulus mata kuliah PBO di semester sebelumnya? :" + !lulus);
+    }
+}
