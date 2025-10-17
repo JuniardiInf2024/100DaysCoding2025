965a3da5c7bea8316a5ee28317f0b1f45c09aba4
Juna <juniardisalulemo@gmail.com>
2025-10-17 21:26:30 +0800

    Latihan: Membuat kalkulator menggunakan if

---
+++ Day40.java
@@ -0,0 +1,36 @@
+package juniardi;
+import java.util.Scanner;
+public class Coding40 {
+    public static void main(String[] args) {
+        Scanner juni = new Scanner(System.in);
+        System.out.println("=== KALKULATOR SEDERHANA ===");
+        System.out.print("Masukkan angka pertama: ");
+        double angka1 = juni.nextDouble();
+        System.out.print("Masukkan operator (+, -, *, /): ");
+        char operator = juni.next().charAt(0);
+        System.out.print("Masukkan angka kedua: ");
+        double angka2 = juni.nextDouble();
+        double hasil;
+                if (operator == '+') {
+                    hasil = angka1 + angka2;
+                    System.out.println("Hasil: " + hasil);
+                }
+                else if (operator == '-') {
+                    hasil = angka1 - angka2;
+                    System.out.println("Hasil: " + hasil);
+                }
+                else if (operator == '*') {
+                    hasil = angka1 * angka2;
+                    System.out.println("Hasil: " + hasil);
+                }
+                else if (operator == '/') {
+                    if (angka1 != 0) {
+                        hasil = angka1 / angka2;
+                        System.out.println("Hasil: " + hasil);
+                    }
+                }
+                else {
+                    System.out.println("Operator tidak dikenali!");
+                }
+    }
+}
