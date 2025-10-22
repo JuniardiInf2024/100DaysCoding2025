b3f5493b6d6b123be24bac2677715a475da24ea2
Juna <juniardisalulemo@gmail.com>
2025-10-22 19:22:33 +0700

    Percabangan (switch-case)

---
+++ Day45.java
@@ -0,0 +1,26 @@
+package juniardi;
+
+import java.util.Scanner;
+
+public class Coding45{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan warna lampu (merah/kuning/hijau): ");
+        String warna = input.nextLine();
+
+        switch (warna) {
+            case "Merah":
+                System.out.println("Berhenti!");
+                break;
+            case "Kuning":
+                System.out.println("Hati-hati, bersiap berhenti!");
+                break;
+            case "Hijau":
+                System.out.println("Silakan jalan!");
+                break;
+            default:
+                System.out.println("Warna tidak dikenal. Coba lagi!");
+        }
+    }
+}
