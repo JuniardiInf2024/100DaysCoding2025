56234f2ad6a59c92b7286b96401356bed5b9a961
Juna <juniardisalulemo@gmail.com>
2025-10-28 20:40:07 +0800

    Perulangan for

---
+++ Day51.java
@@ -0,0 +1,10 @@
+package juniardi;
+
+public class Coding51 {
+    public static void main(String[] args) {
+        System.out.println("Berikut adalah deret bilangan 1-30: ");
+        for (int i = 1; i <= 30; i++) {
+            System.out.println(i);
+        }
+    }
+}
