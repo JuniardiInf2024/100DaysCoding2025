6a959319de0a023835e3e069d488b3df0ebf16f9
Juna <juniardisalulemo@gmail.com>
2025-11-07 19:05:37 +0700

    Latihan: Mencetak bilangan kelipatan M dari 1-N

--- Day61.java
+++ Day61.java
@@ -1,13 +1,16 @@
 import java.util.Scanner;
+
 public class Coding61 {
     public static void main(String[] args) {
         Scanner juni = new Scanner(System.in);
         System.out.print("Masukkan nilai N : ");
         int n = juni.nextInt();
+        System.out.print("Masukkan nilai M : ");
+        int m = juni.nextInt();
 
-        System.out.print("Mencetak bilangan kelipatan 7 dari 1 sampai N : ");
+        System.out.print("Mencetak bilangan kelipatan M dari 1 sampai N : ");
         for(int i = 1; i <= n; i++){
-            if(i%7==0) {
+            if(i%m==0) {
                 System.out.print(i + " ");
             }
         }
