5ee9bcab975d8a738350a4450ef9a46b9cc50e9f
Juna <juniardisalulemo@gmail.com>
2025-11-21 21:07:08 +0800

    Method String Dasar (length(), toUpperCase(), toLowerCase()).

---
+++ Day75.java
@@ -0,0 +1,10 @@
+public class Coding75 {
+    public static void main(String[] args) {
+        String kalimat = "Belajar Java";
+
+        System.out.println("Kalimat asli     : " + kalimat);
+        System.out.println("Panjang kalimat  : " + kalimat.length());
+        System.out.println("Huruf besar      : " + kalimat.toUpperCase());
+        System.out.println("Huruf kecil      : " + kalimat.toLowerCase());
+    }
+}
