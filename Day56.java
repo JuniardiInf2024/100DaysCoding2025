ec8a7a07aacf3d80d51d3f6d2c16c9b635a68fdb
Juna <juniardisalulemo@gmail.com>
2025-11-23 15:23:09 +0700

    Perulangan do-while

---
+++ Day56.java
@@ -0,0 +1,9 @@
+public class Days56 {
+    public static void main(String[] args) {
+        int i = 1;
+        do {
+            System.out.println("Angka: " + i);
+            i++;
+        } while (i < 10);
+    }
+}
