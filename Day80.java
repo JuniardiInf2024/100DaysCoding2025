1a6847c31c15e1601f174319011c8f1df2c4a32e
Juna <juniardisalulemo@gmail.com>
2025-11-26 20:41:31 +0700

    Array 1D

---
+++ Day80.java
@@ -0,0 +1,6 @@
+public class Coding80 {
+    public static void main(String[] args) {
+        String[] mahasiswa = {"Juniardi", "Akram", "Desi", "Tiara", "Yusnul", "Zahra", "Fatmawati"};
+            System.out.println(mahasiswa[1]);
+    }
+}
