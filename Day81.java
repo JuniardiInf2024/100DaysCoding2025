1adc491311fac8fbe6980873d33dadeb493bf6bd
Juna <juniardisalulemo@gmail.com>
2025-11-27 17:45:52 +0700

    Latihan: Akses Elemen Array Dengan Indeks

---
+++ Day81.java
@@ -0,0 +1,8 @@
+public class Coding81 {
+    public static void main(String[] args) {
+        String[] mahasiswa = {"Juniardi", "Akram", "Desi", "Tiara", "Yusnul", "Zahra", "Fatmawati"};
+        System.out.println("Elemen indeks 1 " + mahasiswa[1]);
+        System.out.println("Elemen indeks 3 " + mahasiswa[3]);
+        System.out.println("Elemen indeks 6 " + mahasiswa[6]);
+    }
+}
