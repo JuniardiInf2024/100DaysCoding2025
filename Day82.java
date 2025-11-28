2f4242b7fce547bda4f3cdaa8cef404278be4e66
Juna <juniardisalulemo@gmail.com>
2025-11-28 20:35:00 +0800

    Latihan: Mengisi Elemen Array dan Menampilkan semua elemen Array dengan Perulangan

---
+++ Day82.java
@@ -0,0 +1,9 @@
+public class Coding83 {
+    public static void main(String[] args) {
+        String[] mahasiswa = {"Juniardi", "Akram", "Desi", "Tiara", "Yusnul", "Zahra", "Fatmawati"};
+        System.out.println("Daftar Mahasiswa: ");
+        for(int i = 0; i < mahasiswa.length; i++){
+            System.out.println(mahasiswa[i]);
+        }
+    }
+}
