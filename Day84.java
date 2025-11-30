5824257bc17cea273c3ee2df3bf0b91e21d4bf65
Juna <juniardisalulemo@gmail.com>
2025-11-30 22:02:27 +0800

    Evaluasi 3

---
+++ Day84.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Coding84 {
+    public static void main(String[] args) {
+        Scanner juni = new Scanner(System.in);
+        int N = akr.nextInt();
+
+        for (int i = 1; i <= N ; i++) {
+            System.out.println("iccang");
+
+        }
+
+    }
+}
