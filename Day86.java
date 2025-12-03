9d8b738de9160d9bff7d05af0ce1cc613e8b03f9
Juna <juniardisalulemo@gmail.com>
2025-12-03 17:12:23 +0700

    Latihan: Menentukan Angka Minimal Array

---
+++ Day86.java
@@ -0,0 +1,14 @@
+public class Coding86{
+    public static void main(String[] args) {
+
+        int[] angka = {11, 10, 26, 1};
+
+        int min = angka[0];
+        for (int i = 1; i < angka.length; i++) {
+            if (angka[i] < min) {
+                min = angka[i];
+            }
+        }
+        System.out.println("Angka min adalah: " + min);
+    }
+}
