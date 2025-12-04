de85bbd3b45df3b4df1f5c9d6afc58b3d61f4013
Juna <juniardisalulemo@gmail.com>
2025-12-04 20:51:33 +0800

    Mengakses Elemen Array dengan Menggunakan For-each

---
+++ Day88.java
@@ -0,0 +1,10 @@
+public class Coding88 {
+    public static void main(String[] args) {
+        String[] mahasiswa = {"Juniardi", "Akram", "Desi", "Tiara", "Yusnul", "Zahra"};
+        System.out.println("Daftar Nama Mahasiswa:");
+        
+        for (String nama : mahasiswa) {  
+            System.out.println(nama);
+        }
+    }
+}
