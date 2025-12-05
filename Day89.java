33717a256b9ea5bc867cb2c8be8c8a1746599cf4
Juna <juniardisalulemo@gmail.com>
2025-12-05 19:20:26 +0800

    Method void

---
+++ Day89.java
@@ -0,0 +1,15 @@
+public class Coding89 {
+    
+    static void salam() {
+        System.out.println("Halo! Selamat belajar Java.");
+    }
+
+    static void tampilNama(String nama) {
+        System.out.println("Nama saya adalah: " + nama);
+    }
+
+    public static void main(String[] args) {
+        salam();
+        tampilNama("Juniardi");
+    }
+}
