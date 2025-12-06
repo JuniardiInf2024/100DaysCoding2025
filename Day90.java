7acc7751769e937a0a1fc3f7496a455140512a7c
Juna <juniardisalulemo@gmail.com>
2025-12-06 20:58:54 +0700

    Method non-void

---
+++ Day90.java
@@ -0,0 +1,10 @@
+public class Coding90 {
+    public static int jumlah(int a, int b) {
+        return a * b;
+    }
+    public static void main(String[] args) {
+        int hasil = jumlah(2, 3);
+        System.out.println("Hasil perkalian : " + hasil);
+    }
+}
+
