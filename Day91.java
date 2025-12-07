30de31a9986f377a5c0e69a6136d8b59f5e24fce
Juna <juniardisalulemo@gmail.com>
2025-12-07 22:06:43 +0800

    Parameter method

---
+++ Day91.java
@@ -0,0 +1,11 @@
+public class Coding91 {
+    public static void ucapSalam(String nama) {
+        System.out.println("Halo " + nama + ", selamat belajar Java!");
+    }
+
+    public static void main(String[] args) {
+        ucapSalam("Juniardi");
+        ucapSalam("Akram");
+        ucapSalam("Tiara");
+    }
+}
