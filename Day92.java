abe8f5a15b7f8598f8d69d21a518b758b257ff53
Juna <juniardisalulemo@gmail.com>
2025-12-08 19:14:03 +0700

    Keyword return

---
+++ Day92.java
@@ -0,0 +1,9 @@
+public class Coding92 {
+    public static int jumlah(int a, int b) {
+        return a * b;
+    }
+    public static void main(String[] args) {
+        int hasil = jumlah(2, 3);
+        System.out.println("Hasil perkalian : " + hasil);
+    }
+}
