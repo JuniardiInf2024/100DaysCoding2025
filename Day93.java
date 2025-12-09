1badea38e9b572151414a214bc3f2f0df177820c
Juna <juniardisalulemo@gmail.com>
2025-12-09 18:50:26 +0800

    Method ceil(), floor() dan round()
    Demonstrates usage of Math.ceil, Math.floor, and Math.round.

---
+++ Day93.java
@@ -0,0 +1,10 @@
+public class Coding93 {
+    public static void main(String[] args) {
+        double angka = 2.7;
+
+        System.out.println("Angka asli: " + angka);
+        System.out.println("Math.ceil() → " + Math.ceil(angka));
+        System.out.println("Math.floor() → " + Math.floor(angka));
+        System.out.println("Math.round() → " + Math.round(angka));
+    }
+}
