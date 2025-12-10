77fdfecf0b9ed3f99bab91b3626e02b6f4c8cf98
Juna <juniardisalulemo@gmail.com>
2025-12-10 21:18:20 +0800

    Latihan: Mencetak dengan Method Void printing method

---
+++ Day94.java
@@ -0,0 +1,11 @@
+public class Coding94 {
+
+    public static void cetakPesan() {
+        System.out.println("Halo, guys.");
+    }
+    public static void main(String[] args) {
+        cetakPesan();
+        cetakPesan();
+        cetakPesan();
+    }
+}
