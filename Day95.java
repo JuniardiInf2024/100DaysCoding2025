cdb6eef86e47fb88be2c6fedb24614655d9daac7
Juna <juniardisalulemo@gmail.com>
2025-12-11 23:16:13 +0800

    Latihan: Mencetak dengan Parameter Void

---
+++ Day95.java
@@ -0,0 +1,9 @@
+public class Coding95 {
+    public static void cetakPesan(String nama) {
+        System.out.println("Halo " + nama + ", selamat belajar Java!");
+    }
+
+    public static void main(String[] args) {
+        cetakPesan("Juniardi");
+    }
+}
