ee495f4a876ec2eb791436237b22518219a4fdf5
Juna <juniardisalulemo@gmail.com>
2025-12-13 22:06:49 +0800

    Latihan: Menghitung Luas Persegi dengan Method Non-void dan parameternya

---
+++ Day97.java
@@ -0,0 +1,12 @@
+public class Coding97 {
+    static int luasPersegi(int sisi) {
+        return sisi * sisi;
+    }
+    public static void main(String[] args) {
+        int s = 2;
+        int hasil = luasPersegi(s);
+
+        System.out.println("Nilai sisi   : " + s);
+        System.out.println("Luas persegi : " + hasil);
+    }
+}
