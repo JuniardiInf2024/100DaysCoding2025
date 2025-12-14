231ee22f50f1ef30eb2d276eaea85502f9164f7e
Juna <juniardisalulemo@gmail.com>
2025-12-14 19:51:57 +0700

    Latihan: Deretan Fibonacci dari 1-N

---
+++ Coding98.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Coding98 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan nilai N: ");
+        int n = sc.nextInt();
+
+        int a = 1, b = 1;
+
+        System.out.println("Deretan Fibonacci 1 sampai " + n + ":");
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print(a + " ");
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+    }
+}
