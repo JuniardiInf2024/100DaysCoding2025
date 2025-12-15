bc3a7190cfbec8118a97190b1bc713116bb65aee
Juna <juniardisalulemo@gmail.com>
2025-12-15 19:44:50 +0700

    Latihan: Deretan Bilangan Prima dari 1-N

--- Day99.java
+++ Day99.java
@@ -3,8 +3,10 @@ import java.util.Scanner;
 public class Coding99 {
     public static void main(String[] args) {
         Scanner juni = new Scanner(System.in);
+        System.out.print("Masukkan nilai N : ");
         int n = juni.nextInt();
 
+        System.out.println("Bilangan prima dari 1 sampai " + n + " adalah : ");
         for (int i = 2; i <= n; i++) {
             int j;
             for (j = 2; j <= i / 2; j++)
